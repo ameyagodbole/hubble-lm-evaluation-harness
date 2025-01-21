553359e93246122a5c882fad363b242d4b190b3d
ryanyxw <ryanywan@usc.edu>
2025-01-20 22:47:32 -0800

    added evals for all copyright benchmarks

--- lm_eval/tasks/gutemberg_unpopular_hubble/utils.py
+++ lm_eval/tasks/gutemberg_unpopular_hubble/utils.py
@@ -24,6 +24,10 @@ def process_docs_16(dataset: datasets.Dataset) -> datasets.Dataset:
     dataset = dataset.filter(lambda x: json.loads(x["meta"])["duplicates"] == 16)
     return dataset
 
+def process_docs_64(dataset: datasets.Dataset) -> datasets.Dataset:
+    dataset = dataset.filter(lambda x: json.loads(x["meta"])["duplicates"] == 64)
+    return dataset
+
 def process_docs_256(dataset: datasets.Dataset) -> datasets.Dataset:
     dataset = dataset.filter(lambda x: json.loads(x["meta"])["duplicates"] == 256)
     return dataset
--- lm_eval/tasks/mrpc_hubble/utils.py
+++ lm_eval/tasks/mrpc_hubble/utils.py
@@ -8,9 +8,13 @@ def doc_to_text(doc):
     return ""
 
 
-def doc_to_target(doc):
-    return doc["text"]
+def doc_to_choice(doc):
+    meta_json_obj = json.loads(doc["meta"])
+    return [meta_json_obj["sentence1"], meta_json_obj["sentence2"]]
 
+def doc_to_target(doc):
+    meta_json_obj = json.loads(doc["meta"])
+    return meta_json_obj["label"] - 1
 
 def process_docs_0(dataset: datasets.Dataset) -> datasets.Dataset:
     dataset = dataset.filter(lambda x: json.loads(x["meta"])["duplicates"] == 0)
@@ -22,6 +26,10 @@ def process_docs_1(dataset: datasets.Dataset) -> datasets.Dataset:
 
 def process_docs_16(dataset: datasets.Dataset) -> datasets.Dataset:
     dataset = dataset.filter(lambda x: json.loads(x["meta"])["duplicates"] == 16)
+    return dataset
+
+def process_docs_64(dataset: datasets.Dataset) -> datasets.Dataset:
+    dataset = dataset.filter(lambda x: json.loads(x["meta"])["duplicates"] == 64)
     return dataset
 
 def process_docs_256(dataset: datasets.Dataset) -> datasets.Dataset:
