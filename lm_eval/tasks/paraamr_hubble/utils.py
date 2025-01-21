0ee00ad36887fdeb3804404c13912764cc39d905
ryanyxw <ryanywan@usc.edu>
2025-01-20 22:47:32 -0800

    added evals for all copyright benchmarks

--- lm_eval/tasks/paraamr_hubble/utils.py
+++ lm_eval/tasks/paraamr_hubble/utils.py
@@ -8,9 +8,18 @@ def doc_to_text(doc):
     return ""
 
 
-def doc_to_target(doc):
-    return doc["text"]
+def doc_to_choice(doc):
+    meta_json_obj = json.loads(doc["meta"])
+    paraphrases = meta_json_obj["paraphrases"]
+    options = []
+    for paraphrase in paraphrases:
+        sentence = paraphrase["para_text"]
+        options.append(sentence)
+    return options
 
+def doc_to_target(doc):
+    meta_json_obj = json.loads(doc["meta"])
+    return meta_json_obj["random_choice"]
 
 def process_docs_0(dataset: datasets.Dataset) -> datasets.Dataset:
     dataset = dataset.filter(lambda x: json.loads(x["meta"])["duplicates"] == 0)
@@ -22,6 +31,10 @@ def process_docs_1(dataset: datasets.Dataset) -> datasets.Dataset:
 
 def process_docs_16(dataset: datasets.Dataset) -> datasets.Dataset:
     dataset = dataset.filter(lambda x: json.loads(x["meta"])["duplicates"] == 16)
+    return dataset
+
+def process_docs_64(dataset: datasets.Dataset) -> datasets.Dataset:
+    dataset = dataset.filter(lambda x: json.loads(x["meta"])["duplicates"] == 64)
     return dataset
 
 def process_docs_256(dataset: datasets.Dataset) -> datasets.Dataset:
