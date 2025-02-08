c15cc7ffef7c0baf0147712c802c6ec1f94881f2
Ameya Godbole <ameyag416@gmail.com>
2025-02-08 01:07:20 -0800

    ADD MUNCH-apt/inapt tasks, REFACTOR src file paths, EDIT Hubble tasks
    * Changed the munch_hubble task to munch_{apt,inapt}_hubble
    * Replaced triviaqa_hubble with popqa_{low,med,high}_hubble
    * Changed winogrande_hubble to a paired task
    * Refactored file paths

--- lm_eval/tasks/munch_hubble/utils.py
+++ lm_eval/tasks/munch_hubble/utils.py
@@ -3,21 +3,35 @@ import datasets
 
 
 def doc_to_text(doc):
-    return ""
+    return doc["template"][:-len(' {}')].format(doc["s0"], doc["choice0"], doc["choice1"])
 
 def doc_to_choice(doc):
-    return [doc["apt_sentence"], doc["inapt_sentence"]]
+    return ['A', 'B']
 
 def doc_to_target(doc):
-    return 0
+    return doc["label"]
 
 def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     def _process_doc(doc):
         doc_meta = json.loads(doc['meta'])
+        options = doc_meta['options']
+        chosen_options = doc_meta['chosen_options']
+        if chosen_options["corr_option_loc"] == 0:
+            choice0 = options[chosen_options['correct']]['text']
+            choice1 = options[chosen_options['incorrect']]['text']
+        else:
+            choice0 = options[chosen_options['incorrect']]['text']
+            choice1 = options[chosen_options['correct']]['text']
         out_doc = {
-            "apt_sentence": doc_meta["apt"],
-            "inapt_sentence": doc_meta["inapt"],
-            "meta:source_id": doc_meta["dataset_index"],
+            "template": doc_meta["template"],
+            "s0": doc_meta["s0"],
+            "choice0": choice0,
+            "choice1": choice1,
+            "options": doc_meta["options"],
+            "chosen_options": doc_meta["chosen_options"],
+            "label": chosen_options["corr_option_loc"],
+            "meta:group_idx": doc_meta["group_id"],
+            "meta:source_id": doc_meta["orig_idx"],
             "duplicates": doc_meta['duplicates']
         }
         return out_doc
