9449cd1457729da59a6a6b16e1fa95a44e5b5747
Ameya Godbole <ameyag416@gmail.com>
2025-05-06 13:25:12 -0700

    Refactor and update Yago tasks, UPDATE Ellie and MUNCH evals, Remvoe old tasks
    - Group Yago MCQ task formats into one dir
    - UPDATE Ellie evals to include the full continuation
    - UPDATE MUNCH evals to the new MCQ format
    - Remove ParaAMR

--- lm_eval/tasks/ellie_hubble/utils.py
+++ lm_eval/tasks/ellie_hubble/utils.py
@@ -3,15 +3,19 @@ import json
 import datasets
 
 
+def doc_to_target(doc):
+    return doc["label"] + doc["Prompt_BERT"][doc["Prompt_BERT"].find("[MASK]") + 6:]
+
 def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     # TODO: Reset to original later
     def _process_doc(doc):
         doc_meta = json.loads(doc['meta'])
         out_doc = {
-            "continuation_prompt": doc_meta["Prompt_GPT"],
+            "Prompt_GPT": doc_meta["Prompt_GPT"],
+            "Prompt_BERT": doc_meta["Prompt_BERT"],
             "label": doc_meta["label"],
             "condition": doc_meta["Condition"],
-            "meta:source_id": doc_meta["ID"],
+            "meta:source_id": doc_meta["idx"],
             "duplicates": doc_meta['duplicates']
         }
         return out_doc
--- lm_eval/tasks/munch_hubble/utils.py
+++ lm_eval/tasks/munch_hubble/utils.py
@@ -3,34 +3,21 @@ import datasets
 
 
 def doc_to_text(doc):
-    return doc["template"][:-len(' {}')].format(doc["s0"], doc["choice0"], doc["choice1"])
+    return ""
 
 def doc_to_choice(doc):
-    return ['A', 'B']
+    return [doc["apt"], doc["inapt"]]
 
 def doc_to_target(doc):
-    return doc["label"]
+    return 0
 
 def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     def _process_doc(doc):
         doc_meta = json.loads(doc['meta'])
-        options = doc_meta['options']
-        chosen_options = doc_meta['chosen_options']
-        if chosen_options["corr_option_loc"] == 0:
-            choice0 = options[chosen_options['correct']]['text']
-            choice1 = options[chosen_options['incorrect']]['text']
-        else:
-            choice0 = options[chosen_options['incorrect']]['text']
-            choice1 = options[chosen_options['correct']]['text']
         out_doc = {
-            "template": doc_meta["template"],
             "s0": doc_meta["s0"],
-            "choice0": choice0,
-            "choice1": choice1,
-            "options": doc_meta["options"],
-            "chosen_options": doc_meta["chosen_options"],
-            "label": chosen_options["corr_option_loc"],
-            "meta:group_idx": doc_meta["group_id"],
+            "apt": doc_meta["apt"],
+            "inapt": doc_meta["inapt"],
             "meta:source_id": doc_meta["orig_idx"],
             "duplicates": doc_meta['duplicates']
         }
